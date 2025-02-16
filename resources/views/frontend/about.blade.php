a469c169224dfc0081a4ccf9f0d29675cd9e9e23
Obaydul_Kuasha <obaydul00000@gmail.com>
2025-02-16 21:48:22 +0600

    last update

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.app')
+@section('content')
+    Hello i am about part
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('frontend.home');
 });
+Route::get('/about', function(){
+    return view('frontend.about');
+});
